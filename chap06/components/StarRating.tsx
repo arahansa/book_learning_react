613b1efaefaebb59a5b31b1476a5a6d5387aeeb6
최강용 <me.arahansa@gmail.com>
2022-11-07 23:53:51 +0900

    add chap06

---
+++ chap06/components/StarRating.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {FaStar} from "react-icons/fa";
+
+const StarRating = () => {
+    return (
+        <div>
+            <FaStar color={'red'} />
+        </div>
+    );
+};
+
+export default StarRating;
